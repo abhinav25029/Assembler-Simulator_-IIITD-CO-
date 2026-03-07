18b9036409c1602a0a0cfbc756e2e54ec94b6446
Lakshay Kathuria <lakshaykathuria@Lakshays-MacBook-Air.local>
2026-03-07 21:26:11 +0530

    added seperate code for sub add sltu slt and beq

--- SimpleAssembler/Assembler.py
+++ SimpleAssembler/Assembler.py
@@ -205,3 +205,234 @@ if __name__ == "__main__":
     with open(output_file, "w") as f:
         for line in output:
             f.write(line + "\n")
+
+
+
+def regval(val):
+
+    if val == "zero": 
+        return 0
+    elif val == "ra": 
+        return 1
+    elif val == "sp": 
+        return 2
+    elif val == "gp": 
+        return 3
+    elif val == "tp": 
+        return 4
+    elif val == "t0": 
+        return 5
+    elif val == "t1": 
+        return 6
+    elif val == "t2": 
+        return 7
+    elif val == "s0" or val == "fp": 
+        return 8
+    elif val == "s1": 
+        return 9
+    elif val == "a0": 
+        return 10
+    elif val == "a1": 
+        return 11
+    elif val == "a2": 
+        return 12
+    elif val == "a3": 
+        return 13
+    elif val == "a4": 
+        return 14
+    elif val == "a5": 
+        return 15
+    elif val == "a6": 
+        return 16
+    elif val == "a7": 
+        return 17
+    elif val == "s2": 
+        return 18
+    elif val == "s3": 
+        return 19
+    elif val == "s4": 
+        return 20
+    elif val == "s5": 
+        return 21
+    elif val == "s6": 
+        return 22
+    elif val == "s7": 
+        return 23
+    elif val == "s8": 
+        return 24
+    elif val == "s9": 
+        return 25
+    elif val == "s10": 
+        return 26
+    elif val == "s11": 
+        return 27
+    elif val == "t3": 
+        return 28
+    elif val == "t4": 
+        return 29
+    elif val == "t5": 
+        return 30
+    elif val == "t6": 
+        return 31
+
+#funct7 rs2 rs1 funct3 rd opcode
+
+def r_type(ins,rs2,rs1,rd):
+    x = ""
+    if ins == "add":
+        x = x+"0000000"
+
+        rs2call = regval(rs2)
+        rs2bin = format(rs2call, '05b')
+        x = x+rs2bin
+
+        rs1call = regval(rs1)
+        rs1bin = format(rs1call, '05b')
+        x = x+rs1bin
+
+        x = x+"000"
+
+        rdcall = regval(rd)
+        rdbin = format(rdcall, '05b')
+        x = x+rdbin
+        
+        x = x+"0110011"
+    
+    elif ins == "sub":
+        x = x+"0100000"
+
+        rs2call = regval(rs2)
+        rs2bin = format(rs2call, '05b')
+        x = x+rs2bin
+
+        rs1call = regval(rs1)
+        rs1bin = format(rs1call, '05b')
+        x = x+rs1bin
+
+        x = x+"000"
+
+        rdcall = regval(rd)
+        rdbin = format(rdcall, '05b')
+        x = x+rdbin
+        
+        x = x+"0110011"
+
+
+    elif ins == "slt":
+        x = x+"0000000"
+
+        rs2call = regval(rs2)
+        rs2bin = format(rs2call, '05b')
+        x = x+rs2bin
+
+        rs1call = regval(rs1)
+        rs1bin = format(rs1call, '05b')
+        x = x+rs1bin
+
+        x = x+"010"
+
+        rdcall = regval(rd)
+        rdbin = format(rdcall, '05b')
+        x = x+rdbin
+        
+        x = x+"0110011"
+
+    elif ins == "sltu":
+
+        x = x+"0000000"
+
+        rs2call = regval(rs2)
+        rs2bin = format(rs2call, '05b')
+        x = x+rs2bin
+
+        rs1call = regval(rs1)
+        rs1bin = format(rs1call, '05b')
+        x = x+rs1bin
+
+        x = x+"011"
+
+        rdcall = regval(rd)
+        rdbin = format(rdcall, '05b')
+        x = x+rdbin
+        
+        x = x+"0110011"
+    print(x)
+
+#imm[12] imm[10:5] rs2 rs1 funct3 imm[4:1] imm[11] opcode
+
+def b_type(ins,imm_int,rs2,rs1):
+    x = ""
+    if ins == "beq":
+        imm_bin = format(imm_int & 0x1FFF, '013b')
+        x = x+imm_bin[0]
+        x = x+imm_bin[2:8]
+
+        rs2call = regval(rs2)
+        rs2bin = format(rs2call, '05b')
+        x = x+rs2bin
+
+        rs1call = regval(rs1)
+        rs1bin = format(rs1call, '05b')
+        x = x+rs1bin
+
+        x = x+"000"
+
+        x = x+imm_bin[8:12]
+        x = x+imm_bin[1]
+
+        x = x+"1100011"
+    print(x)
+
+with open("riscv_program.txt") as f:
+    x = f.readlines()
+
+l = []
+j = 0 
+for i in x:
+    i = i.strip()
+    i = i.replace(","," ")
+    i = i+" "+str(j)
+    j = j+1
+    l.append(i)
+    
+
+#add,sub,sub2,slt,sltu,beq
+
+for i in l:
+    k = i.split(" ")
+
+
+    if k[0] in ["add","sub","slt","sltu"]:
+        r_type(k[0],k[3],k[2],k[1])
+    elif k[1] in ["add","sub","slt","sltu"]:
+        r_type(k[1],k[4],k[3],k[2])
+
+
+    if k[0] in ["beq"]:
+        if k[3].isdigit():
+            imm = int(k[3])
+        else:
+            store = k[3]
+            current_idx = k[4]
+            for u in l:
+                z = u.split(" ")
+
+                if z[0] == store+":":
+                    label_idx = z[5]
+            imm = (int(label_idx) - int(current_idx))*4
+        b_type(k[0],imm,k[2],k[1])
+
+    elif k[1] in ["beq"]:
+        if k[4].isdigit():
+            imm = int(k[4])
+        else:
+            store = k[4]
+            current_idx = k[5]
+            for u in l:
+                z = u.split(" ")
+                if z[0] == store+":":
+                    label_idx = z[5]
+            imm = (int(label_idx) - int(current_idx))*4
+        b_type(k[1],imm,k[3],k[2])
+    
+
